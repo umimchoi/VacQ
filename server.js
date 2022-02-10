4beff7b8a0a27fdde493fbfe418f77b6c193415e
Umim Choi <umimchoi@gmail.com>
2022-02-10 14:43:40 +0700

    Connect with MongoDB atlas

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config({ path: "./config/config.env" });
+
+connectDB();
+
+const hospitals = require("./routes/hospitals");
+const app = express();
+app.use(express.json());
+app.use("/api/v1/hospitals", hospitals);
+
+const PORT = process.env.PORT || 5000;
+const server = app.listen(
+  PORT,
+  console.log("Server running in", process.env.NODE_ENV, " mode on port ", PORT)
+);
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  server.close(() => process.exit(1));
+});
