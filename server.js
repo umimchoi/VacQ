7844a688de5e538b557da616d96bce230533edb2
Umim Choi <umimchoi@gmail.com>
2022-02-22 21:54:36 +0700

    Authentication

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
 const connectDB = require("./config/db");
 
 dotenv.config({ path: "./config/config.env" });
 
 connectDB();
-
-const hospitals = require("./routes/hospitals");
 const app = express();
+const hospitals = require("./routes/hospitals");
+const auth = require("./routes/auth");
+
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/v1/hospitals", hospitals);
+app.use("/api/v1/auth", auth);
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(
